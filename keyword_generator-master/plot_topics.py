23bd9dbf4e7838f9b68d875dd49ae99be9f7c110
Cygnuhss <jelmervannuss@yahoo.com>
2016-09-30 19:27:52 +0200

    Add visualization sources

---
+++ keyword_generator-master/plot_topics.py
@@ -0,0 +1,5 @@
+import matplotlib as plt
+
+# Source: https://de.dariah.eu/tatom/topic_model_visualization.html
+# Source: https://de.dariah.eu/tatom/visualizing_trends.html
+# Source: http://chrisstrelioff.ws/sandbox/2014/11/13/getting_started_with_latent_dirichlet_allocation_in_python.html
