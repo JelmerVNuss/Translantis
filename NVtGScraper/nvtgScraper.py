c28cbc0143846d79f8e00f9d460f25acd918173a
Cygnuhss <jelmervannuss@yahoo.com>
2016-10-28 21:25:50 +0200

    Add NVtG Scraper

--- NVtGScraper/nvtgScraper.py
+++ NVtGScraper/nvtgScraper.py
@@ -15,83 +15,29 @@ import bs4
 
 validFilenameChars = "-_.() {}{}".format(string.ascii_letters, string.digits)
 
-
+BASE_FOLDER = "/NVtG/"
 # Where to find the article files.
 SEARCH_BOX_URL = ("https://www.ntvg.nl/search/advanced?search=&page=", "&in=full&author_options=0")
 ARTICLE_INFO_URL = ("https://www.ntvg.nl/artikelen/", "/artikelinfo")
 ARTICLE_INFO_SELECTOR = "#block-system-main > div > div > div > div.field.field-name-field-year.field-type-number-integer.field-label-hidden > div > div"
-DOWNLOAD_SELECTOR = "#block-ntvgmod-print > div > div > ul > li.last > a"
+DOWNLOAD_URL_SELECTOR = "#block-ntvgmod-print > div > div > ul > li.last > a"
+DOWNLOAD_CLASS = "download jquery-once-1-processed"
 
 # Scrape only these pages.
+BEGIN_PAGE = 0
+# This is the first page with a downloadable PDF. This will change as articles
+# are being added.
 BEGIN_PAGE = 593
+# Update the end page to the current end page.
 END_PAGE = 31046
 ARTICLES_PER_PAGE = 10
-START_ARTICLE = "een-meisje-met-rugklachten"
 
-https://www.ntvg.nl/search/advanced?search=&page=593&in=full&author_options=0
-https://www.ntvg.nl/artikelen/een-meisje-met-rugklachten/artikelinfo
+#https://www.ntvg.nl/search/advanced?search=&page=593&in=full&author_options=0
+#https://www.ntvg.nl/artikelen/een-meisje-met-rugklachten/artikelinfo
 
 def removeDisallowedFilenameChars(filename):
     cleanedFilename = str(unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore'))
     return ''.join(c for c in cleanedFilename[1:] if c in validFilenameChars)
-
-
-class Song():
-    """A Song consists of a title, artist, ranking and score."""
-    def __init__(self, title, artist, ranking, score):
-        self.title = title
-        self.artist = artist
-        self.ranking = ranking
-        self.score = score
-
-    def __repr__(self):
-        return "Song(title: \"{0}\", artist: \"{1}\", ranking: {2}, score: {3})".format(self.title, self.artist, self.ranking, self.score)
-
-    @property
-    def label(self):
-        """Return the label for this song."""
-        label_string = "_".join([str(self.ranking), str(self.title), str(self.artist), str(self.score)])
-        return removeDisallowedFilenameChars(label_string.replace(" ", "-"))
-
-
-def scrapeSongLabels(song_ranking_url, verbose=False):
-    """Retrieve the songs and their labels.
-    Return the labels as a list of tuples in the format:
-        (song title, artist, ranking)
-
-    Succeeds only if the website is online, otherwise returns an error.
-    """
-    res = checkWebsiteAvailability(song_ranking_url)
-
-    if verbose:
-        print("Website successfully requested: {}".format(song_ranking_url))
-
-    labels = []
-
-    if verbose:
-        print("Parsing HTML for {} songs...".format(TOP_N))
-
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-    for i in range(1, TOP_N + 1):
-        elemsTitle = soup.select("#chart-list > ol > li:nth-of-type({}) > div.clearfix > div.titlecredit-wrapper.pull-left > a > span.title".format(i))
-        title = elemsTitle[0].text
-
-        elemsArtist = soup.select("#chart-list > ol > li:nth-of-type({}) > div.clearfix > div.titlecredit-wrapper.pull-left > a > span.credit".format(i))
-        artist = elemsArtist[0].text
-
-        elemsScore = soup.select("#chart-list > ol > li:nth-of-type({}) > div.clearfix > div.titlecredit-wrapper.pull-left > div.title-detail-view-container > div > div.title-stats.row-fluid > div:nth-of-type(3) > strong".format(i))
-        score = elemsScore[0].text
-
-        ranking = i
-
-        song = Song(title, artist, ranking, score)
-        labels.append(song)
-
-    if verbose:
-        print("Done labelling {} songs...".format(TOP_N))
-
-    return labels
 
 
 def checkWebsiteAvailability(url):
@@ -104,29 +50,88 @@ def checkWebsiteAvailability(url):
     return res
 
 
-def getArticleInfo(article_url):
-    return articleName, articleId
+def findArticleNames(page, verbose=False):
+    """Find the article names on this page.
+    """
+    article_url = SEARCH_BOX_URL[0] + str(page) + SEARCH_BOX_URL[1]
+    res = checkWebsiteAvailability(article_url)
 
-def getDownloadUrl(article_url):
-    pass
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    articleNames = []
+    for ul in soup.findAll('ul', class_='content-list'):
+        for a in ul.findAll('a'):
+            articleNames.append(a.attrs['href'].split('/')[-1])
+    if verbose:
+        print("On page {}, found article names {}.".format(page, articleNames))
+
+    return articleNames
 
 
+def getArticleInfo(articleName, verbose=False):
+    """Return the article ID and publishing year based on an article name.
+    The article ID is usually of the form VOLUME;ISSUE, but this might introduce
+    errors while saving.
+    The files are thus stored as VOLUME-ISSUE.
+    """
+    article_info_url = ARTICLE_INFO_URL[0] + articleName + ARTICLE_INFO_URL[1]
+    res = checkWebsiteAvailability(article_info_url)
 
-def downloadFromUrl(url, file_name):
+    articleId = ""
+    year = ""
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    try:
+        articleInfo = soup.select(ARTICLE_INFO_SELECTOR)[0]
+        splitText = articleInfo.text.replace(';', ' ').split()
+        articleId = removeDisallowedFilenameChars(splitText[-1].replace(':', '-'))
+        year = splitText[-2]
+
+        if verbose:
+            print("Found article ID [{}] in year [{}].".format(articleId, year))
+    except IndexError:
+        if verbose:
+            print("Skipping this article [{}]: no information available.".format(articleName))
+
+    return articleId, year
+
+
+def getDownloadUrl(articleName, verbose=False):
+    article_url = ARTICLE_INFO_URL[0] + articleName
+    res = checkWebsiteAvailability(article_url)
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    try:
+        download_url = soup.find('a', class_='download').attrs['href']
+        if verbose:
+            print("Found download url [{}].".format(download_url))
+    except:
+        download_url = ""
+        if verbose:
+            print("Skipping this article [{}]: no downloadable PDF found.".format(articleName))
+
+    return download_url
+
+def downloadFromUrl(url, file_name, verbose=False):
     # Open in binary mode.
     with open(file_name, "wb") as file:
-        response = requests.get(url)
+        response = checkWebsiteAvailability(url)
         file.write(response.content)
+    if verbose:
+        print("File successfully downloaded: {}".format(file_name))
 
 
-def downloadFile(article_url, year, path=".", verbose=False):
+def downloadFile(articleName, path=".", verbose=False):
     """Download the file to the downloads folder."""
-    article = getArticleInfo(article_url)
-    file_path = path + "/" + year + "/" + article + ".pdf"
+    articleId, year = getArticleInfo(articleName, verbose)
+    file_path = path + BASE_FOLDER + str(year)
+    file_name =  "/" + articleId + ".pdf"
 
-    if os.path.isfile(file_path):
+    if os.path.isfile(file_path + file_name):
         if verbose:
-            print("File already exists: {}".format(file_path))
+            print("File already exists: {}".format(file_path + file_name))
         return
     try:
         os.makedirs(file_path)
@@ -134,28 +139,20 @@ def downloadFile(article_url, year, path=".", verbose=False):
         if not os.path.isdir(file_path):
             raise
 
-    download_url = getDownloadUrl(article_url)
-    print("Downloading to: {}".format(file_path))
-    downloadFromUrl(download_url, file_path)
+    download_url = getDownloadUrl(articleName, verbose)
+    if download_url:
+        if verbose:
+            print("Downloading to: {}".format(file_path + file_name))
+        downloadFromUrl(download_url, file_path + file_name, verbose)
 
 
 if __name__ == "__main__":
-    file_path = year
-    downloadFromUrl("https://www.ntvg.nl/system/files/publications/1919106010001a.pdf", file_path)
-    # for i in range(BEGIN_PAGE, END_PAGE + 1):
-    #     for j in range(1, ARTICLES_PER_PAGE + 1):
-    #         print("\nWeek {} ({})".format(i, CURRENT_YEAR))
-    #         print("------------------")
-    #         DATE_PATH = "{}/week-{}".format(CURRENT_YEAR, i)
-    #         song_ranking_url = SONG_RANKING_URL + DATE_PATH
-    #
-    #         print("\nWeek {} ({})".format(i, CURRENT_YEAR))
-    #         print("------------------")
-    #         for song in week_label:
-    #             print(song)
-    #             downloadSong(song, date_path="{}-{}/".format(CURRENT_YEAR, i), verbose=True)
-    #
-    # best_song = labels[0][1][0]
-    # query = " ".join([best_song.title, best_song.artist])
-    # search_result = youtube_search.find_first(query)
-    # print(search_result)
+    verbose = False
+    for page in range(BEGIN_PAGE, END_PAGE):
+        if page % 10 == 0:
+            print("Now at page {}...".format(page))
+        articleNames = findArticleNames(page, verbose)
+        for articleName in articleNames:
+            downloadFile(articleName, verbose=verbose)
+
+    print("All articles from Nederlands Tijdschrift voor Geneeskunde are scraped.")
