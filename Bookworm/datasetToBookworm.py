d37fc92b7a22cf3678fdbb36a14284dd5833e9bf
Cygnuhss <jelmervannuss@yahoo.com>
2017-03-07 13:13:33 +0100

    Adjust bookworm for Accountant and ESB

--- Bookworm/datasetToBookworm.py
+++ Bookworm/datasetToBookworm.py
@@ -2,56 +2,101 @@ import os
 import shutil
 
 
-ROOT = "Elseviers_Magazine_txt"
+# Defines how many characters of the first line of an article should be taken
+# as the searchstring field.
 SEARCHSTRING_LENGTH = 50
 
 
-# Extract the metadata.
-dates = {}
-searchstrings = {}
-folders = [ROOT] + ["{}/{}".format(ROOT, year) for year in range(2007, 2011+1)]
-for folder in folders:
-    for filename in os.listdir(folder):
+def extractMetadata(dataset, root):
+    dates = {}
+    searchstrings = {}
+
+    if dataset == "elsevier":
+        folders = [root] + ["{}/{}".format(root, year) for year in range(2007, 2011+1)]
+    elif dataset == "esb":
+        folders = [root]
+    elif dataset == "accountant":
+        folders = [root]
+    else:
+        raise ValueError("Dataset not implemented: {}".format(dataset))
+
+    for folder in folders:
+        for filename in os.listdir(folder):
+            filename, extension = os.path.splitext(filename)
+            if extension == ".txt":
+                if dataset == "elsevier":
+                    year, month, day = filename[-8:-4], filename[-4:-2], filename[-2:]
+                elif dataset == "esb":
+                    year, month, day = filename[:4], None, None
+                elif dataset == "accountant":
+                    year, month, day = filename, None, None
+                else:
+                    raise ValueError("Dataset not implemented: {}".format(dataset))
+                date = [year, month, day]
+                date = [element for element in date if element]
+                dates[filename] = "-".join(date)
+
+                with open("{}/{}{}".format(folder, filename, extension), "r", encoding="utf-8") as f:
+                    try:
+                        searchstring = f.readline()[:SEARCHSTRING_LENGTH]
+                    except KeyError:
+                        searchsting = f.readline()
+                    searchstrings[filename] = searchstring
+    metadata = [dates, searchstrings]
+    return metadata
+
+def createBookwormFolders(dataset):
+    if not os.path.exists('{}/metadata'.format(dataset)):
+        os.makedirs('{}/metadata'.format(dataset))
+    if not os.path.exists('{}/texts/raw'.format(dataset)):
+        os.makedirs('{}/texts/raw'.format(dataset))
+
+def createFieldDescriptionsFile(dataset):
+    with open('{}/metadata/field_descriptions.json'.format(dataset), 'w+') as fieldDescriptionsFile:
+        text = """[
+        {"datatype": "searchstring", "field": "searchstring", "unique": true, "type": "text"},
+        {"datatype": "categorical", "field": "type", "unique": false, "type": "text"},
+        {"datatype": "categorical", "field": "author", "unique": false, "type": "text"},
+        {"datatype": "categorical", "field": "genre", "unique": false, "type": "text"},
+        {"datatype": "time", "field": "date_month", "unique": true, "type": "integer"}
+    ]"""
+        fieldDescriptionsFile.write(text)
+
+def createJsonCatalogFile(dataset, root, metadata):
+    dates, searchstrings = metadata
+    for filename in os.listdir(root):
         filename, extension = os.path.splitext(filename)
         if extension == ".txt":
-            year, month, day = filename[-8:-4], filename[-4:-2], filename[-2:]
-            dates[filename] = "-".join([year, month, day])
+            with open('{}/metadata/jsoncatalog.txt'.format(dataset), 'a+') as jsonFile:
+                filenameMeta = '"filename": "{}"'.format(filename)
+                dateMeta = '"date": "{}"'.format(dates[filename])
+                searchstringMeta = '"searchstring": "{}"'.format(searchstrings[filename])
+                metadata = [filenameMeta, dateMeta, searchstringMeta]
+                jsonEntry = "{{{}}}\n".format(", ".join(metadata))
+                jsonFile.write(jsonEntry)
 
-            with open("{}/{}{}".format(folder, filename, extension), "r", encoding="utf-8") as f:
-                try:
-                    searchstring = f.readline()[:SEARCHSTRING_LENGTH]
-                except KeyError:
-                    searchsting = f.readline()
-                searchstrings[filename] = searchstring
+def copyFilesToBookworm(dataset, root):
+    for filename in os.listdir(root):
+        filename, extension = os.path.splitext(filename)
+        if extension == ".txt":
+            originalPath = '{}/{}{}'.format(root, filename, extension)
+            destinationPath = '{}/texts/raw/{}'.format(dataset, filename + '.txt')
+            shutil.copyfile(originalPath, destinationPath)
 
 
-if not os.path.exists('elsevier/metadata'):
-    os.makedirs('elsevier/metadata')
-if not os.path.exists('elsevier/texts/raw'):
-    os.makedirs('elsevier/texts/raw')
+if __name__ == "__main__":
+    # Create Elsevier Bookworm
+    #dataset = "elsevier"
+    #root = "Elseviers_Magazine_txt"
+    # Create ESB Bookworm
+    dataset = "esb"
+    root = "ESB_text"
+    # Create Accountant Bookworm
+    #dataset = "accountant"
+    #root = "accountant_txt"
 
-# Create a field descriptions file.
-with open('elsevier/metadata/field_descriptions.json', 'w+') as fieldDescriptionsFile:
-    text = """[
-    {"datatype": "searchstring", "field": "searchstring", "unique": true, "type": "text"},
-    {"datatype": "categorical", "field": "type", "unique": false, "type": "text"},
-    {"datatype": "categorical", "field": "author", "unique": false, "type": "text"},
-    {"datatype": "categorical", "field": "genre", "unique": false, "type": "text"},
-    {"datatype": "time", "field": "date_month", "unique": true, "type": "integer"}
-]"""
-    fieldDescriptionsFile.write(text)
-
-# Create the json catalog file and copy text files.
-for filename in os.listdir(ROOT):
-    filename, extension = os.path.splitext(filename)
-    if extension == ".txt":
-        with open('elsevier/metadata/jsoncatalog.txt', 'a+') as jsonFile:
-            filenameMeta = '"filename": "{}"'.format(filename)
-            dateMeta = '"date": "{}"'.format(dates[filename])
-            searchstringMeta = '"searchstring": "{}"'.format(searchstrings[filename])
-            metadata = [filenameMeta, dateMeta, searchstringMeta]
-            jsonEntry = "{{{}}}\n".format(", ".join(metadata))
-            jsonFile.write(jsonEntry)
-        originalPath = '{}/{}{}'.format(ROOT, filename, extension)
-        destinationPath = 'elsevier/texts/raw/{}'.format(filename + '.txt')
-        shutil.copyfile(originalPath, destinationPath)
+    metadata = extractMetadata(dataset, root)
+    createBookwormFolders(dataset)
+    createFieldDescriptionsFile(dataset)
+    createJsonCatalogFile(dataset, root, metadata)
+    copyFilesToBookworm(dataset, root)
